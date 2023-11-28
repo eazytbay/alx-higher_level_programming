a9e903177a9dd341b79d157f66984f648706bafe
eazytbay <eazytbay@gmail.com>
2023-11-28 06:06:38 -0800

    8-uppercase.py task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/bin/bash/python3
+def uppercase(s):
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            print("{}".format(chr(ord(char) - 32)), end="")
+        else:
+            print("{}".format(char), end="")
+    print()
