56280aca3fbf1fe11bad1690ca51b9e558428188
eazytbay <eazytbay@gmail.com>
2023-11-30 05:21:26 -0800

    2-args.py updated and corrected

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-    
 if __name__ == "__main__":
-    """DIsplay the exact list and number of the arguments"""
     import sys
     n_arg = len(sys.argv) - 1
-
     if n_arg == 0:
         print("0 arguments.")
     elif n_arg == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(n_arg))
-
     for i in range(n_arg):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
