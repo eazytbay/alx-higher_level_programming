6ef11afd0b248a0ab8f1f92f2cdcdf6f26bc959c
eazytbay <eazytbay@gmail.com>
2023-11-30 03:44:44 -0800

    corrected again

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def add(a, b):
     return a + b
+
 if __name__ == "__main__":
     a = 1
     b = 2
-    from add_0 import add as add_0
 
-    result = add_0(a, b)
+    from add_0 import add
+
+    result = add(a, b)
 
     print("{} + {} = {}".format(a, b, result))
