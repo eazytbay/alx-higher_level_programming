0fa3ef727129d1f6240675342380f9a7d9c71c0a
eazytbay <eazytbay@gmail.com>
2023-11-30 05:10:35 -0800

    2-args.py task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+    
+if __name__ == "__main__":
+    import sys
+    n_arg = len(sys.argv) - 1
+
+    if n_arg == 1:
+        print("0 arguments.")
+    elif n_arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n_arg))
+
+    for i in range(n_arg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
