275a509af0b4a43b20053119fc7eda8ff061becc
eazytbay <eazytbay@gmail.com>
2023-11-30 13:41:58 -0800

    102-magic_calculation.py task

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+def magic_calculation(x, y):
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).add, __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).sub
+
+    if x < y:
+        result = add(x, y)
+        for i in range(4, 6):
+            result = add(result, i)
+        return result
+    else:
+        return sub(x, y)
