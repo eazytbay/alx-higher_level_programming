2828be097125b5d1482c46e8d800e39062d3a1d9
eazytbay <eazytbay@gmail.com>
2023-11-30 13:45:51 -0800

    102-magic_calculation.py corrected

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 def magic_calculation(x, y):
     add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).add, __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).sub
 
