1a3e565e993d08e3aab6f9ba74eafe71adc2adc2
eazytbay <eazytbay@gmail.com>
2023-12-02 10:48:50 -0800

    6-print_matrix_integer.py changed corrected and updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join(map(str, row)))
+    for mov in matrix:
+        print(" ".join("{:d}".format(x) for x in mov))
