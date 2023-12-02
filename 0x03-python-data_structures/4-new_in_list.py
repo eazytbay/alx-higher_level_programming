99b54bb240777662d089445771e77db31b2aa625
eazytbay <eazytbay@gmail.com>
2023-12-02 01:52:53 -0800

    4-new_in_list.py updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copied_list = my_list.copy()
-  # Checks the index if it is negative
     if idx < 0:
         return my_list.copy()
-  # Checks the index if it is out of range
     if idx > len(my_list) - 1:
         return my_list.copy()
     else:
-  # This Replaces the element at the index specified
         copied_list[idx] = element
         return copied_list
