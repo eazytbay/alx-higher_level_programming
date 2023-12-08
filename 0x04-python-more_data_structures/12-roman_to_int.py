61fb3670ecc9a0f52b8fb182db7e72e9d24b144b
eazytbay <eazytbay@gmail.com>
2023-12-08 02:50:55 -0800

    12-roman_to_int.py corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,12 +5,10 @@ def roman_to_int(roman_string):
     digit = (('I', 1), ('V', 5), ('X', 10),
                 ('L', 50), ('C', 100), ('D', 500),
                 ('M', 1000))
-
     if type(roman_string) is not str:
         return 0
     if roman_string is None:
         return 0
-
     for item in reversed(roman_string):
         for elem in digit:
             if item == elem[0]:
