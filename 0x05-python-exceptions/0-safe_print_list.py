63ec0c18f99df6e184a910ffae86727d76b512cd
eazytbay <eazytbay@gmail.com>
2023-12-18 03:57:25 -0800

    0x05. Python - Exceptions corrected

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     digit = 0
     try:
-        for a in range(x):
-            print("{:d}".format(my_list[a]), end="")
-            digit = digit + 1
-    except:
-        print()
-        return digit
-    else:
-        print()
-        return digit
+        for a in range(0, x):
+            print(my_list[a], end="")
+            digit += 1
+    except (IndexError, TypeError):
+        pass
+    print()
+    return digit
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+import sys
+
+
 def magic_calculation(a, b):
     computed_output = 0
     for x in range(1, 3):
         try:
             if x > a:
-                raise Exception("Too far")
+                raise Exception('Too far')
             else:
-                computed_output += (a**b) / x
-        except:
+                computed_output += (a ** b) / x
+        except Exception as error:
             computed_output = b + a
             break
     return computed_output
