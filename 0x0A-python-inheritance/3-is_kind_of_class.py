7805a2030ba986571aaa632ef40cb5718a88bfab
eazytbay <eazytbay@gmail.com>
2024-01-08 06:34:06 -0800

    0x0A. Python - Inheritance updated

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""
-A module that Contains the is_kind_of_class function
+"""A Module is_kind_of_class
+Confirms if the object is an instance of, or if it is an
+instance of a class that inherited from, the particular class
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """Returns True provided obj is an instance or inherited 
-    from a_class, else False"""
-    return (isinstance(obj, a_class))
+    """Confirms if obj is an instance of a_class or a class
+    inherited from a_class
+    Args:
+        - obj: The object to look at or check
+        - a_class:  The class to evaluate 
+    Returns: True if the obj is an instance of a class else False
+    """
+
+    return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""
-A module Containing the inherits_from function
-"""
+"""Module inherits_from
+Checks if the object is an instance of a class that inherited
+(whether inherited directly/indirectly) from the specified class"""
 
 
 def inherits_from(obj, a_class):
-    """returns true provided obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Confirms if obj is an instance of a class that
+    inherited from a_class
+    Args:
+        - obj: The particular object to look at
+        - a_class: The class to compute
+    Returns: Trueif the obj is a subclass of a_class else reutrn False
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
