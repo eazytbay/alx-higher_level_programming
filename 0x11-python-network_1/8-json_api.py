4377bd7bb5a2c63384ccdc10535e34d563fd4b57
eazytbay <eazytbay@gmail.com>
2024-03-28 06:09:36 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,17 +6,17 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     if len(argv) == 2:
-        y = argv[1]
+        q = argv[1]
     else:
-        y = ""
-    x = requests.post('http://0.0.0.0:5000/search_user', data={'y': y})
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        x_dict = x.json()
-        id = x_dict.get('id')
-        name = x_dict.get('name')
-        if len(x_dict) == 0 or not id or not name:
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
             print("No result")
         else:
-            print("[{}] {}".format(x_dict.get('id'), x_dict.get('name')))
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
     except:
         print("Not a valid JSON")
