8a7a7cba3e14dd80767a120562b1c50c2f396dff
eazytbay <eazytbay@gmail.com>
2024-03-28 05:46:35 -0700

    3-error_code.py updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     x = request.Request(argv[1])
     try:
         with request.urlopen(x) as r:
-            print(r.read().decode('utf-8'))
+            print(x.read().decode('utf-8'))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code))
