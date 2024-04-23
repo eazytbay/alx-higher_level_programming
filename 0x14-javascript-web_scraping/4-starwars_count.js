02c80663d21291279f0dd99ee2c18cb49343a2f5
eazytbay <eazytbay@gmail.com>
2024-04-23 13:13:34 -0700

    0-readme.js and 4-starwars updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,8 +7,8 @@ request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const rslt = JSON.parse(body).rslt;
-    console.log(rslt.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
